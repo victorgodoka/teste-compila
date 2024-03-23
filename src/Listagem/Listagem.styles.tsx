c4ec9ce8ea98130a24b79416ed3914e9c1ec983d
Victor Martins <martinsvictorsantos@gmail.com>
2024-03-23 14:22:49 -0300

    feat: criado componente de listagem

--- src/Listagem/Listagem.styles.tsx
+++ src/Listagem/Listagem.styles.tsx
@@ -7,14 +7,20 @@ export const Contato = styled.li`
   border-bottom: 1px solid #ccc;
   line-height: 1.5;
   font-size: 1.2rem;
-`
+  display: flex;
+  justify-content: space-between;
+
+  & a {
+    margin-right: 12px;
+  }
+`;
 
 export const ListagemWrapper = styled.ul`
   background-color: #15172b;
   border-radius: 20px;
   min-height: 100vh;
   padding: 20px;
-  width: 320px;
+  width: 640px;
   margin: 0 auto;
 `
 
@@ -23,4 +29,14 @@ export const Letter = styled.div`
   font-weight: bold;
   border-bottom: rgba(0, 0, 0, 0.222);
   margin-bottom: 12px;
+`;
+
+export const Linha = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 12px;
+
+  & h2 {
+    font-size: 2rem;
+  }
 `;
--- src/Listagem/index.tsx
+++ src/Listagem/index.tsx
@@ -1,30 +1,64 @@
-import React from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
-import { Contato, Letter, ListagemWrapper } from "./Listagem.styles";
+import { Contato, Letter, Linha, ListagemWrapper } from "./Listagem.styles";
+import { Input } from "../CriarEditarContato/CriarEditarContato.style";
 
 export default function Listagem() {
+  const [listagemFinal, setListagem] = useState<ListagemContatos>();
   const listagem = useLoaderData() as Contatos[];
 
-  let data = listagem.reduce((r: any, e: any) => {
+  let data = (array:any) => array.reduce((r: any, e: any) => {
     let letra = e.nome[0];
     if (!r[letra]) r[letra] = { letra, contatos: [e] };
     else r[letra].contatos.push(e);
     return r;
   }, {});
 
+  useEffect(() => {
+    setListagem(data(listagem));
+    console.log(listagemFinal);
+  }, []);
+
+  const buscarContatos = (event: { target: { name: any; value: any } }) => {
+    const { value } = event.target;
+    const busca = listagem.filter((val) => val.nome.toLowerCase().includes(value.toLowerCase()));
+    setListagem(data(busca));
+  }
+
+  const ApagarContato = (id: any) => {
+    if (confirm("Deseja apagar este contato?")) {
+      return fetch(`http://localhost:3000/contatos/${id}`, {
+        method: "DELETE",
+      });
+    }
+  };
+
   return (
     <ListagemWrapper>
-      <li>Contatos</li>
-      <li>
-        <Link to="criar">Criar contato</Link>
-      </li>
-      {Object.keys(data).map((key) => (
-        <>
+      <Input placeholder="Busca" onChange={buscarContatos} />
+      <Linha>
+        <li>
+          <h2>Contatos</h2>
+        </li>
+        <li>
+          <Link to="criar">Criar contato</Link>
+        </li>
+      </Linha>
+      {listagemFinal && Object.keys(listagemFinal).map((key: string) => (
+        <Fragment key={key}>
           <Letter>{key}</Letter>
-          {data[key].contatos.map(({ id, nome }: any) => (
-            <Contato>{nome}</Contato>
+          {listagemFinal[key]?.contatos.map(({ id, nome }: any) => (
+            <Contato key={id}>
+              <Link to={`contato/${id}`}>{nome}</Link>
+              <div>
+                <Link to={`editar/${id}`}>Editar</Link>
+                <a href="#" onClick={() => ApagarContato(id)}>
+                  Apagar
+                </a>
+              </div>
+            </Contato>
           ))}
-        </>
+        </Fragment>
       ))}
     </ListagemWrapper>
   );
