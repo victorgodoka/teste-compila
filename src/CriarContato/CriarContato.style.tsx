3aa169d84852a1b45ee1af962564c2183d3b2c38
Victor Martins <martinsvictorsantos@gmail.com>
2024-03-23 10:50:08 -0300

    feat: criação do criar contato

---
+++ src/CriarContato/CriarContato.style.tsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+  background-color: #15172b;
+  border-radius: 20px;
+  height: 500px;
+  padding: 20px;
+  width: 320px;
+  margin: 0 auto;
+`;
+
+export const Input = styled.input`
+  background-color: #303245;
+  border-radius: 8px;
+  border: 0;
+  color: #eee;
+  font-size: 1.355rem;
+  height: 100%;
+  outline: 0;
+  margin-bottom: 12px;
+  padding: 8px 12px;
+  width: 100%;
+`;
+
+export const Label = styled.label`
+  cursor: pointer;
+  margin-bottom: 12px;
+  font-size: .955rem;
+`
+
+export const Button = styled.button`
+  background-color: #08d;
+  border-radius: 12px;
+  border: 0;
+  box-sizing: border-box;
+  color: #eee;
+  cursor: pointer;
+  font-size: 18px;
+  height: 50px;
+  margin-top: 38px;
+  // outline: 0;
+  text-align: center;
+  width: 100%;
+`;
